2e842f6e67fcd8045e0caf905ba0140ae5eb402b
Lukas Geiger <lukas.geiger94@gmail.com>
2018-10-15 10:08:49 +0200

    Add bundle script
    Flow types defs don't support import/export from other defs: https://github.com/flow-typed/flow-typed/blob/master/CONTRIBUTING.md#dont-import-types-from-other-libdefs
    
    Thus we need to bundle everything.

---
+++ bundle.js
@@ -0,0 +1,15 @@
+const dts = require("dts-bundle");
+const { join } = require("path");
+
+const entryPoints = ["", "ajax", "operators", "testing", "webSocket"];
+const baseDir = ["node_modules", "@reactivex", "rxjs", "dist", "typings"];
+
+entryPoints.forEach(entryPoint => {
+  dts.bundle({
+    name: join("rxjs", entryPoint),
+    main: join(...baseDir, entryPoint, "index.d.ts"),
+    baseDir: join(...baseDir),
+    out: join(__dirname, "dist", entryPoint, "index.d.ts"),
+    indent: "	"
+  });
+});
